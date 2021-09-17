41f98d1791e120fc75fcd26911cf0bbbee9c6808
lillyCode <lilly_code@outlook.com>
2021-09-17 08:58:31 +0100

    day 1 100 days of code: set up react project

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <div>
+                <h2>Expense Item</h2>
+            </div>
+            <div>
+                <p>this is the expense item</p>
+            </div>
+           
+        </div>
+    );
+}
+
+export default ExpenseItem;
