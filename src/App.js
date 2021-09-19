57746a72e444da3cce1eea14f63b5c4639366673
lillyCode <lilly_code@outlook.com>
2021-09-19 23:55:42 +0100

    day 1: expense item to own component & css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses';
 
 function App() {
+
   return (
-    <div className="App">
-     <h2>This is a test level 2 header</h2>
-     <p>this is a test paragraph</p>
-     <ExpenseItem></ExpenseItem>
-    </div>
+   <div>
+      <Expenses items={Expenses}/>
+   </div>
   );
 }
 
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,16 @@
-function ExpenseItem() {
+import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+import Card from './Card';
+
+function ExpenseItem(props) {
     return (
-        <div>
-            <div>
-                <h2>Expense Item</h2>
+        <Card className="expense-item" >
+            <ExpenseDate date={props.date}/>
+            <div className="expense-item__description" >
+                <h2>{props.title}</h2>
+                <div className="expense-item__price" > ${props.amount} </div>
             </div>
-            <div>
-                <p>this is the expense item</p>
-            </div>
-           
-        </div>
+        </Card>
     );
 }
 
