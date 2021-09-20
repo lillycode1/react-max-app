0af017eb541b0020b99f88d1c33c5f3662ab53dd
lillyCode <lilly_code@outlook.com>
2021-09-20 08:39:07 +0100

    day2: 100days, starting ch4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
-import Expenses from './components/Expenses';
+import Expenses from './components/Expenses/Expenses';
 
 function App() {
+  const expenses = [
+    { title: 'Car insurance', amount: 234.23, date: new Date(2021, 2,23)},
+    { title: 'Home insurance', amount: 134.33, date: new Date(2021, 2,26)},
+    { title: 'Fire insurance', amount: 34.55, date: new Date(2021, 2,20)},
+  ];
 
   return (
    <div>
-      <Expenses items={Expenses}/>
+      <Expenses items={expenses}/>
    </div>
   );
 }
